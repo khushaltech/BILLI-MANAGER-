85fcc5e5c3ff59720b3b981f1763add660af6eeb
ALONE <116359861+ALTRON-XD@users.noreply.github.com>
2023-02-15 22:19:10 +0530

    ɴᴏᴛʜɪɴɢ

--- KRISTY/modules/tts.py
+++ KRISTY/modules/tts.py
@@ -33,44 +33,8 @@ def gtts(update, context):
     finally:
         if os.path.isfile("k.mp3"):
             os.remove("k.mp3")
-
-
-# Open API key
-API_KEY = "6ae0c3a0-afdc-4532-a810-82ded0054236"
-URL = "http://services.gingersoftware.com/Ginger/correct/json/GingerTheText"
-
-
-@run_async
-@typing_action
-def spellcheck(update, context):
-    if update.effective_message.reply_to_message:
-        msg = update.effective_message.reply_to_message
-
-        params = dict(lang="US", clientVersion="2.0", apiKey=API_KEY, text=msg.text)
-
-        res = requests.get(URL, params=params)
-        changes = json.loads(res.text).get("LightGingerTheTextResult")
-        curr_string = ""
-        prev_end = 0
-
-        for change in changes:
-            start = change.get("From")
-            end = change.get("To") + 1
-            suggestions = change.get("Suggestions")
-            if suggestions:
-                sugg_str = suggestions[0].get("Text")  # should look at this list more
-                curr_string += msg.text[prev_end:start] + sugg_str
-                prev_end = end
-
-        curr_string += msg.text[prev_end:]
-        update.effective_message.reply_text(curr_string)
-    else:
-        update.effective_message.reply_text(
-            "ʀᴇᴘʟʏ ᴛᴏ ꜱᴏᴍᴇ ᴍᴇꜱꜱᴀɢᴇ ᴛᴏ ɢᴇᴛ ɢʀᴀᴍᴍᴀʀ ᴄᴏʀʀᴇᴄᴛᴇᴅ ᴛᴇxᴛ ʙᴀʙʏ🥀!"
-        )
-
+            
 dispatcher.add_handler(DisableAbleCommandHandler("tts", gtts, pass_args=True))
-dispatcher.add_handler(DisableAbleCommandHandler("splcheck", spellcheck))
 
 __help__ = """
  » `/tts` <reply to msg> :  ᴛᴜʀɴꜱ ᴛᴇxᴛ ᴛᴏ ꜱᴘᴇᴇᴄʜ
